8a548ac4346a84c789303afcdd055270014911d7
Fengxiang Xue <47923440+frankxue831@users.noreply.github.com>
2025-07-03 22:15:12 +0800

    Initialize Spring Boot project with Maven wrapper and basic application structure

---
+++ src/main/java/org/workshop/learn_proxy/LearnProxyApplication.java
@@ -0,0 +1,13 @@
+package org.workshop.learn_proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnProxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/workshop/learn_proxy/LearnProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.workshop.learn_proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
